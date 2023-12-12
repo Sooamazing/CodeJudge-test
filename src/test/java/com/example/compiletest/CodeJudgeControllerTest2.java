734401ee5da3c5f41bec03cb80fd76dd4ecc86d3
goorm <honang515@gmail.com>
2023-12-13 01:11:11 +0900

    12/13 기록 겸 README.md 추가, README만 추가 안 되는 것 같아 test 이름변경

--- src/test/java/com/example/compiletest/CodeJudgeControllerTest2.java
+++ src/test/java/com/example/compiletest/CodeJudgeControllerTest2.java
@@ -6,17 +6,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.mockito.Mockito.when;
 
 @WebMvcTest(CodeJudgeController.class)
-public class CodeJudgeControllerTest_2 {
+public class CodeJudgeControllerTest2 {
 
 	@InjectMocks
 	private CodeJudgeController codeJudgeController;
